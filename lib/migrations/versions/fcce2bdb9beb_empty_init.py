73e9fc6a911065f38a64709ead375507899c019a
Joe Milius <joemilius@gmail.com>
2023-01-18 16:46:08 -0700

    Completed

---
+++ lib/migrations/versions/fcce2bdb9beb_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fcce2bdb9beb
+Revises: 
+Create Date: 2023-01-18 14:50:54.676043
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fcce2bdb9beb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
